b9e4a0a96a87e170a3e81aa109a5f2ee070521a8
Joe Wood <joe9663@gmail.com>
2016-05-07 11:22:45 -0400

    adds function for multiple employees, alos cleans up code formatting to make it more readable

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,22 +1,26 @@
-puts "What is your name?"
-name= gets.chomp
-puts "How old are you in years?"
-age_response= gets.chomp
-age= age_response.to_i
-puts "What year were you born?"
-year_response= gets.chomp
-year= year_response.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic= gets.chomp
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-insurance= gets.chomp
-if age= 2016-year && (garlic= "y" || insurance= "y") 
-	puts "Probably not a vampire."
-elsif age!= 2016-year && (garlic= "n" || insurance= "n")
-	puts "Probably a vampire."
-elsif age!= 2016-year && garlic= "n" && insurance= "n"
-	puts "Almost certainly a vampire."
-elsif name= "Drake Cula" || name= "Tu Fang"
-	puts "Definitely a vampire."
-else puts "Results inconclusive."
+count=0
+puts "How many people are we screening?"
+employee_count= gets.chomp.to_i
+while count<employee_count
+	puts "What is your name?"
+	name= gets.chomp.to_s
+	puts "How old are you in years?"
+	age= gets.chomp.to_i
+	puts "What year were you born?"
+	year= gets.chomp.to_i
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	garlic= gets.chomp.to_s
+	puts "Would you like to enroll in the company's health insurance? (y/n)"
+	insurance= gets.chomp.to_s
+	count+=1
+	if (age== 2016-year) && (garlic== "y" || insurance== "y")==true 
+		puts "Probably not a vampire."
+	elsif (age!= 2016-year) && (garlic== "n" || insurance== "n")==true
+		puts "Probably a vampire."
+	elsif (age!= 2016-year && garlic== "n" && insurance== "n")==true
+		puts "Almost certainly a vampire."
+	elsif (name== "Drake Cula" || name== "Tu Fang")==true
+		puts "Definitely a vampire."
+	else puts "Results inconclusive."
+	end
 end
