04c6a48d2e9fb2c5b355b827a81e90d47f85ade2
Joe Wood <joe9663@gmail.com>
2016-05-08 21:27:15 -0400

    adds hamster data feedback

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,12 +1,16 @@
 puts "What is the hamster's name?"
-name=gets.chomp
+	name=gets.chomp
 puts "On a scale of 1 to 10, how noisy is this hamster?"
-volume=gets.chomp.to_i
+	volume=gets.chomp.to_i
 puts "What color is this hamster's fur?"
-color=gets.chomp
+	color=gets.chomp
 puts "Is the hamster a good candidate for adoption? (y/n)"
-adopt=gets.chomp
+	adopt=gets.chomp
 puts "What is the estimated age of the hamster?"
-age=gets.chomp
-if age.empty? == true then age=nil end
-	
+	age=gets.chomp.to_i
+	if age.empty? == true then age=nil end
+puts "Name: #{name}"
+puts "Noise Level: #{volume}"
+puts "Color: #{color}"
+puts "Adoption Canadicy: #{adopt}"
+puts "Estimated Age: #{age}"
