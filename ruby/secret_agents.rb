5857aac5042ec3827b64b4c17b22417ffe15fa75
Joe Wood <joe9663@gmail.com>
2016-05-09 11:16:54 -0400

    adds comments to increase readability

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,8 +1,9 @@
-#Define encrypt method that indexex every letter in the password.
+#Define encrypt method that indexes every letter in the password.
 #Runs the .next function on each letter.
 def encrypt(password)
     count=0
     while count<password.length
+# Allows the encrypt method to account for blank spaces
        if password[count]== " "
        print " "
 #Edge case for when "z" would return aa       
@@ -14,17 +15,22 @@ def encrypt(password)
     end
 end
 
-#Decrypt runs reverse of the next function.
-
+#Decrypt runs reverse of the next function
+#No equivalents reverse function for .next
+#New logic must be created
 def decrypt(password)
+#Creates the alphabet as a string for easier access and less confusing code
 	alphabet="abcdefghijklmnopqrstuvwxyz "
 	count=0
 	while count<password.length
+#Determines where a particular letter in password fits in the alphabet
 		magic= alphabet.index(password[count])
 		if magic== " "
 			print " "
+#Edge case for when decrypting "a" would not return a usable value
 		elsif alphabet[magic -1]== "a"
 			print "z"
+#Rolls back each letter to the previous one in the alphabet
 		else print alphabet[magic -1]
 		count +=1
 		end
