c79faaa05ebd2d906ee336a940135447caf4f55f
Joe Wood <joe9663@gmail.com>
2016-05-10 11:39:03 -0400

    creates string variable for methods

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -4,17 +4,22 @@
 #Runs the .next function on each letter.
 def encrypt(password)
     count=0
+    encoded=""
     while count<password.length
 # Allows the encrypt method to account for blank spaces
        if password[count]== " "
        print " "
+       encoded=encoded + " "
 #Edge case for when "z" would return aa       
        elsif password[count]== "z"
        	print "a"
+       encoded=endoded + "a"
        else print password[count].next
+       encoded=encoded + password[count].next
        end
         count +=1
     end
+    return encoded
 end
 
 #Decrypt runs reverse of the next function
@@ -24,23 +29,27 @@ def decrypt(password)
 #Creates the alphabet as a string for easier access and less confusing code
 	alphabet="abcdefghijklmnopqrstuvwxyz "
 	count=0
+	decoded=""
 	while count<password.length
 #Determines where a particular letter in password fits in the alphabet
 		magic= alphabet.index(password[count])
 		if magic== " "
+			decoded=decoded + " "
 			print " "
 #Edge case for when decrypting "a" would not return a usable value
-		elsif alphabet[magic -1]== "a"
+		elsif alphabet[magic-1]== "a"
+			decoded=decoded + "z"
 			print "z"
 #Rolls back each letter to the previous one in the alphabet
 		else print alphabet[magic -1]
-		count +=1
+			decoded=decoded + alphabet[magic-1]
 		end
+		count+=1
 	end
+	return decoded
 end
 
 #DRIVE CODE
-
 decrypt(encrypt("swordfish"))
 #Asks user what the program will do
 puts "Would you like to encrypt or decrypt a password?"
@@ -53,11 +62,10 @@ end
 if response== "encrypt"
 	puts "What password are we encrypting?"
 	password=gets.chomp
-	print encrypt(password)
+	encrypt(password)
 elsif response== "decrypt"
 	puts "What password are we decrypting?"
 	password=gets.chomp
-	print decrypt(password)
+	decrypt(password)
 else puts "Error"
 end
-exit
