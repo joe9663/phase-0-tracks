3b53cd6f74d69f2f7d4b4d25b58984381bc5628f
Joe Wood <joe9663@gmail.com>
2016-05-21 17:44:48 -0400

    creates puppy class with appropriate methods in class and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(n)
+#   puts "Woof! " * n
+    n.times {puts "Woof!"}
+  end
+  
+  def roll_over
+    puts "Roll over!"
+  end
+  
+  def dog_years(years)
+    return years * 7
+  end
+  
+  def hungry?
+    return true
+  end
 end
 
+spot = Puppy.new
+spot.fetch("bone")
+spot.speak(5)
+spot.roll_over
+spot.dog_years(4)
+spot.hungry?
