08a3d6fba8e1c4b852437ec4bef486dd5445e8d4
Joe Wood <joe9663@gmail.com>
2016-05-21 18:39:09 -0400

    adds characteristics and behavior of puppies

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+PUPPIES
+-----------------------
+Characteristics
+-----------------------
+Size: Varies on breed
+Cute: Yes
+Color: Varies on breed
+Name: Up to the owner
+-----------------------
+Behavior
+-----------------------
+Barks all the time
+Chews on everything
+Naps constantly
+Loves to play
