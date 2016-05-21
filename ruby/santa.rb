7f7a61b542d7b52df336bc8d072cb55fa5a9e93e
Joe Wood <joe9663@gmail.com>
2016-05-21 19:55:51 -0400

    creates array of Santa class instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,12 @@
 class Santa
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -6,12 +14,17 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
-
-	def initialize
-		puts "Initializing Santa instance ..."
-	end
 end
 
-kringle = Santa.new
-kringle.speak
-kringle.eat_milk_and_cookies("chocolate chip")
+#kringle = Santa.new
+#kringle.speak
+#kringle.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
