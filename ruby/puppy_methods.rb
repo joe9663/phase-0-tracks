4f0581d2b586384c0784d9cdd41babe4aa28da4e
Joe Wood <joe9663@gmail.com>
2016-05-21 18:19:03 -0400

    adds new kitten class with methods and driver code for testing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -34,3 +34,32 @@ spot.speak(5)
 spot.roll_over
 spot.dog_years(4)
 spot.hungry?
+
+class Kitten
+  
+  def initialize
+    puts "Initializing new kitten class..."
+  end
+  
+  def meow(n)
+    n.times {puts "MEOW!!!!!!"}
+  end
+  
+  def hunt(animal)
+    puts "I brought you this dead #{animal}! Aren't you proud of me?"
+  end
+  
+end
+
+kitten_list = []
+n = 1
+50.times {
+  kitten = Kitten.new
+  kitten_list << kitten
+  n += 1
+  }
+
+kitten_list.each  { |kitten|
+  kitten.meow(1)
+  kitten.hunt("bird") 
+  }
