a625f201b5780c838a236cb4024164cdbdf524f2
Joe Wood <joe9663@gmail.com>
2016-05-21 21:03:37 -0400

    creates getter and setter methods for Santa class. Provides driver code to test the new methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,6 +14,25 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
+	#Setter methods for Santa class
+	def celebrate_birthday
+		@age = @age += 1
+	end
+	def get_mad_at=(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking.insert(-1, reindeer)
+		p @reindeer_ranking
+	end
+	def gender_bender=(gender)
+		@gender = gender
+	end
+	#Getter Methods for Santa class
+	def age
+		@age
+	end
+	def ethnicity
+		@ethnicity
+	end
 end
 
 #kringle = Santa.new
@@ -25,6 +44,10 @@ example_genders = ["agender", "female", "bigender", "male", "female", "gender fl
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+  end
 
-p santas
+p santas[1].gender_bender=("male")
+p santas[1].celebrate_birthday
+p santas[1].get_mad_at= "Vixen"
+p santas[1].age
+p santas[1].ethnicity
