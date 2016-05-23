00d14143cc3243f3bda672b61476a4915712bc4f
Joe Wood <joe9663@gmail.com>
2016-05-23 00:34:51 -0400

    adds user interface and print results for user to see

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -8,28 +8,60 @@
 
 class Car
 
-	def initialize(color, miles)
-		@passengers = []
+attr_reader :color
+attr_accessor :driver, :mileage, :insurance
+
+	def initialize(color)
 		@color = color
-		@miles = miles
-		puts "Adding the new #{@color} car with #{@miles} miles to the car database."
+		puts "Adding the new #{@color} car to the car database."
 	end	
 
-	def drive=(miles)
-		@mileage += miles
+	def odometer=(miles)
+		@mileage = miles
 	end
 
-	def rider=(passenger_name)
-		@passengers << passenger_name
-		puts "#{passenger_name} got into the car."
+	def driver=(driver_name)
+		@driver = driver_name
 	end
 
 	def insurance_check
 		if @insurance == true
-			puts "This car is insured, have fun driving!"
-		else puts "Please get this car insured before you drive it."
+			puts "Looks like this car is insured, have fun driving!"
+		else puts "Please get this car insured before you take it out for a drive."
 		end
 	end
 end
 
-civic = Car.new("green", 120000)
+
+car_array = []
+continue = ""
+until continue == "done"
+	puts "What is the color of this new car?"
+		color = gets.chomp
+		new_car = Car.new(color)
+	puts "How many miles has this car been driven?"
+		miles = gets.chomp.to_i
+		new_car.odometer=(miles)
+	puts "Who is driving this car?"
+		driver_name = gets.chomp
+		new_car.driver=(driver_name)
+	puts "Does this car have insurance? (y/n)"
+		response = gets.chomp
+		if response != "y"
+			@insurance = false
+		else
+			@insurance = true
+		end
+		new_car.insurance_check
+	car_array << new_car
+	puts "If you're done adding cars type 'done', otherwise hit enter and let's do it again!"
+		continue = gets.chomp
+end
+
+#ObjectSpace.each_object Car do |car|
+#	p car.color
+#	p car.miles
+#	p car.driver
+#	p car.insurance
+#end
+p car_array
