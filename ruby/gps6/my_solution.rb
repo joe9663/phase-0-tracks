42d4c06b4d0ea91b220cdea66e75c4c1a04d338d
Joe Wood <joe9663@gmail.com>
2016-05-25 13:50:03 -0400

    cleans up code

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,62 +1,52 @@
-# Virus Predictor
-
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
-
-# EXPLANATION OF require_relative
-#
-#
-require_relative 'state_data'
-
 class VirusPredictor
 
+  # Initializes the class and establishes instance variables
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # Runs the combined methods of predicted_deaths and speed_of_spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  # Takes in arguments of population_density, population, and state
+  # Depending on population density, it calculates a rounded number of deaths
+  # Prints string with state and number of deaths
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
+    percentage = 0.4 if @population_density >= 200
+    percentage = 0.3 if @population_density >= 150
+    percentage = 0.2 if @population_density >= 100
+    percentage = 0.1 if @population_density >= 50
+    percentage = 0.05 if @population_density < 50
+    
+    number_of_deaths = (@population * percentage).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  
+  # Takes in arguments of population_density and state
+  # Depending on population_density, speed is increased
+  # prints continued string of how many months it will take to spread across the state
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
+    speed += 0.5 if @population_density >= 200
+    speed += 1 if @population_density >= 150
+    speed += 1.5 if @population_density >= 100
+    speed += 2 if @population_density >= 50      
+    speed += 2.5 if @population_density < 49
+    
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
@@ -69,19 +59,7 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
-
-
-#=======================================================================
-# Reflection Section
+STATE_DATA.each {|state, population|
+  state = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
+  state.virus_effects
+  }
