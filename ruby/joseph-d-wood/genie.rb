46297d91290342bfe93e6f19569b411b3da524ef
Joe Wood <joe9663@gmail.com>
2016-05-30 20:23:00 -0400

    adds grant wish method for genie

--- ruby/joseph-d-wood/genie.rb
+++ ruby/joseph-d-wood/genie.rb
@@ -8,6 +8,7 @@ class Genie
 	def initialize(name, age)
 		@name = name
 		@age = age
+# Submitted wishes attribute is a hash that stores the wish and whether or not it was granted.
 		@submitted_wishes = {}
 	end
 # Instance method that allows the genie to state their name.
@@ -15,8 +16,28 @@ class Genie
 	def appear
 		puts "*POOF!* I am #{@name}, and you have three wishes!"
 	end
+
+	def grant_wish(wish)
+# First the method checks to make sure three wishes have not already been granted.
+# If granted wishes is less than 3 then wish is granted and added to the list.		
+		if @submitted_wishes.length < 3
+			@submitted_wishes[wish] = true
+			puts "Your wish of #{wish} has been granted!"
+			return true
+# If 3 wishes have already been granted then the wish is added with a false value to the hash.		
+		elsif @submitted_wishes.length >= 3
+			@submitted_wishes[wish] = false
+			puts "Sorry, I've already granted three wishes today!"
+			return false
+		
+		else
+		end			
+	end
 end
 # Driver code
 test = Genie.new("Joe", 28)
 
-test.appear
+test.grant_wish("dog")
+test.grant_wish("pony")
+test.grant_wish("train set")
+test.grant_wish("helicopter")
