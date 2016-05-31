d6b37f945a6403f921876e2f7214bee1a713c2c0
Joe Wood <joe9663@gmail.com>
2016-05-31 01:22:40 -0400

    creates an object with horse name and color pairs

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,4 +1,10 @@
 var colors = ["blue", "red", "green", "yellow"];
 var names = ["Ed", "George", "Hank", "Larry"];
-colors + "purple"
-names + "Max"
+colors.push ("purple")
+names.push("Max")
+
+var horses = {};
+for (var i = 0; i < names.length; i++) {
+	horses[names[i]] = colors[i];
+} 
+console.log(horses)
