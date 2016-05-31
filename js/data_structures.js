9c6bb1b14df97308d6b504a5d3f8c2be3f726cf2
Joe Wood <joe9663@gmail.com>
2016-05-31 01:35:13 -0400

    creates car objects that behave like class instances

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,3 +8,26 @@ for (var i = 0; i < names.length; i++) {
 	horses[names[i]] = colors[i];
 } 
 console.log(horses)
+
+function Car(color, isHybrid, doors) {
+	this.color = color;
+	this.isHybrid = isHybrid;
+	this.doors = doors;
+
+	this.vroom = function() {console.log("VROOM!!");};
+}
+
+var one_car = new Car("blue", true, 2);
+console.log(one_car);
+console.log("There it goes out of the shop!");
+one_car.vroom();
+
+var two_car = new Car("green", false, 4);
+console.log(one_car);
+console.log("There it goes out of the shop!");
+one_car.vroom();
+
+var three_car = new Car("red", false, 2);
+console.log(one_car);
+console.log("There it goes out of the shop!");
+one_car.vroom();
