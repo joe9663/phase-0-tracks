e56e75aa4f88f3f0ba18aabf4054a2f077325b38
Joe Wood <joe9663@gmail.com>
2016-05-30 20:05:56 -0400

    creates genie class and initialize and appear methods

---
+++ ruby/joseph-d-wood/genie.rb
@@ -0,0 +1,21 @@
+# Create a new class.
+# Allow for name and age parameters in the initialize method
+class Genie
+# Allow age to be readable
+# Allow name to be readable and writeable
+	attr_reader :age
+	attr_accessor :name
+	def initialize(name, age)
+		@name = name
+		@age = age
+	end
+# Instance method that allows the genie to state their name.
+# This method also informs the user that they have 3 wishes.
+	def appear
+		puts "*POOF!* I am #{@name}, and you have three wishes!"
+	end
+end
+# Driver code
+test = Genie.new("Joe", 28)
+
+test.appear
