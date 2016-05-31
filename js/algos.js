000b887b57a9c6f14621a0fed3664db6fdce4fb1
Joe Wood <joe9663@gmail.com>
2016-05-31 02:00:05 -0400

    compares string lengths and prints the longest one

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+// Make the array and iterate the length function on each string in the array.
+// Then the length of each string needs to be compared to others.
+// Using comparators && for both to be true will print the one that is the longest.
+// The longest string is then printed to the console.
+
+var phrases = ["long phrase", "longest phrase", "longer phrase"];
+
+if (phrases[0].length > phrases[1].length && phrases[0].length > phrases[2].length) {
+	console.log(phrases[0]);
+}
+else if (phrases[1].length > phrases[0].length && phrases[1].length > phrases[2].length) {
+	console.log(phrases[1]);
+}
+else if (phrases[2].length > phrases[0].length && phrases[2].length > phrases[1].length) {
+	console.log(phrases[2]);
+}
