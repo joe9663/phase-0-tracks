e384290d5935b5a37687793cd70b2390dd1213bb
Joe Wood <joe9663@gmail.com>
2016-06-05 23:27:44 -0400

    adds loop for user to add more sandwiches to the database

--- databases/sandwich-rater/sandwich.rb
+++ databases/sandwich-rater/sandwich.rb
@@ -22,7 +22,27 @@ db.execute(create_sandwich_table)
 # asks the user what sandwich they would like to add and what their rating of it is
 puts "What is the sandwich you would like to add?"
 	sandwich_name = gets.chomp
+
 puts "How many stars out of 5 would you give this sandwich?"
 	sandwich_rating = gets.chomp.to_i
 
 	db.execute("INSERT INTO sandwiches (name, rating) VALUES (?, ?)", [sandwich_name, sandwich_rating])
+
+# begins a loop to add more sandwiches to the database
+puts "Would you like to add any more sandwiches to the list?"
+	response = gets.chomp.downcase
+
+until response == "no"
+
+	puts "What is the sandwich you would like to add?"
+		sandwich_name = gets.chomp
+
+	puts "How many stars out of 5 would you give this sandwich?"
+		sandwich_rating = gets.chomp.to_i
+
+	db.execute("INSERT INTO sandwiches (name, rating) VALUES (?, ?)", [sandwich_name, sandwich_rating])
+
+	puts "Would you like to add any more sandwiches to the list?"
+		response = gets.chomp.downcase
+
+end
